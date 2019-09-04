9e7a66f6a8b92ea11ab298fdb55e58aff714073e
unknown <daniel.fragoso@redich.mx>
2019-09-04 17:53:14 -0500

    se finalizo el ejercicio de TextoScreen

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,17 @@
 import React, {useState} from 'react';
 import { View, StyleSheet, TextInput } from 'react-native';
 import {Text, Input} from 'react-native-elements';
-import {STYLE_ELEMENT_ID} from "react-native-web/dist/exports/StyleSheet/constants";
 
 const TextScreen = () =>
 {
     const [name, setName] = useState("");
     const [name2, setName2] = useState("");
+
+    const labelDanger = <Text style={style.textColorDanger}>Favor de ingresar 5 o más caracteres</Text>;
+    const labelSucess = <Text style={style.textColorSucess}>Contraseña valida.</Text>
     return(
         <View>
+            <Text>Ingrese su contraseña:</Text>
             <Input
                 placeholder='INPUT WITH ICON'
                 leftIcon={{ type: 'font-awesome', name: 'user'}}
@@ -18,7 +21,7 @@ const TextScreen = () =>
                 }
             />
             <TextInput
-                style={style.iconStyle}
+                style={style.inputStyle}
                 autoCapitlize="none"
                 autoCorrect={false}
                 value={name}
@@ -27,16 +30,33 @@ const TextScreen = () =>
                 }
             />
             <Text>Input 1: {name2}</Text>
+            { (name2.length < 5)
+                ? <Text style={style.textColorDanger}>Favor de ingresar 5 o más caracteres</Text>
+                : <Text style={style.textColorSucess}>Contraseña valida.</Text>
+            }
             <Text>Input 2: {name}</Text>
+            {
+                (name.length < 5)
+                    ? labelDanger
+                    : labelSucess
+            }
+
         </View>
 
     );
 }
+
 const style = StyleSheet.create({
-    iconStyle:{
+    inputStyle:{
         margin: 10,
         borderColor: 'black',
         borderWidth: 0.5
+    },
+    textColorDanger:{
+        color: 'red'
+    },
+    textColorSucess:{
+        color: 'green'
     }
 })
 export default TextScreen;
